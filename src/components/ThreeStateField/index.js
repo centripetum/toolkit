ec81a7499162b8cc5f8c28dd5baea8d5f2c590b5
Matt Norris <matthew.norris96@gmail.com>
2019-03-19 01:57:06 +1300

    chore: added 2nd input and toggle so only 1 box can be selected

--- src/components/ThreeStateField/index.js
+++ src/components/ThreeStateField/index.js
@@ -5,14 +5,29 @@ import styled from 'styled-components'
 const { useEffect, useState } = React
 
 const handleChange = ({ target = {} }) => {
-  console.log('changed')
+  console.log('Changed: ' + target.id)
+  if (target.id === 'box1') {
+    // if checkbox box2 selected deselect it
+    if (document.getElementById('box2').checked) {
+      document.getElementById('box2').checked = false
+    }
+  }
+  if (target.id === 'box2') {
+    // if checkbox box1 selected deselect it
+    if (document.getElementById('box1').checked) {
+      document.getElementById('box1').checked = false
+    }
+  }
 }
 
-function ThreeStateField ({ name }) {
+function ThreeStateField () {
   return (
     <div>
       Yes
-      <input name={name} type='checkbox' onChange={handleChange} />
+      <input id='box1' type='checkbox' onChange={handleChange} />
+      <br />
+      No
+      <input id='box2' type='checkbox' onChange={handleChange} />
     </div>
   )
 }
